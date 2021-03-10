8574280838cf24b665d98905d9a4d6c256f2f163
John Su <js@Johns-MacBook-Pro.local>
2021-03-10 17:42:26 -0500

    Temporary fix for hardcoded credentials: set them as environmental variables before running instead

--- apps/backend/src/app/recommendations/recommendations.service.ts
+++ apps/backend/src/app/recommendations/recommendations.service.ts
@@ -2,11 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { PersonalizeRuntime } from "aws-sdk";
 
 const REGION = 'us-east-2';
-// put these variables in a file somewhere else or set as environmental variables when deployed
-const ACCESS_KEY = 'AKIA33S27WUXQ2JIL2V7'
-const SECRET_KEY = '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'
 const CAMPAIGN_ARN = 'arn:aws:personalize:us-east-2:815160866095:campaign/descart-basic'
-
+// SET AWS_ACCESS_KEY_ID AND AWS_SECRET_ACCESS_KEY AS ENVIRONMENT VARIABLES
 
 @Injectable()
 export class RecommendationsService {
@@ -29,7 +26,7 @@ export class RecommendationsService {
       numResults: this.NUM_RECS}, 
       function(err, data) {
         for (var i = 0; i < data.itemList.length; i++) {
-          nums.add(parseInt(data.itemList[i].itemId));
+          nums.add(Number(data.itemList[i].itemId));
         }
     })
     return [...nums];
